bcd1db5948535c8f52e9b842158abb8566e29f6f
Richard Padilla <padillarichard2021@berkeley.edu>
2019-10-22 01:19:38 -0700

    lol

--- minicontest2/myTeam.py
+++ minicontest2/myTeam.py
@@ -560,7 +560,7 @@ class DefensiveAgent(CaptureAgent):
         return gameState.isRed(pos)
     def getWeights(self, gameState, action):
         # Set this manually
-        return {"num_opps_in_territory":1,"num_food_in_territory":10,"is_in_enemy":-1000000000000}
+        return {"num_opps_in_territory":1,"num_food_in_territory":10,"is_in_enemy":-1000000000000,"min_dist":5}
 
     def getFeatures(self, gameState, action):
         # figure out good features here
@@ -578,6 +578,7 @@ class DefensiveAgent(CaptureAgent):
             sum_opps=sum(friends_in_our_territory)
         friendly_food = sum([sum(i) for i in self.getFood(new_gamestate)])
         is_in_opp_ground=0
-        if(self.is_in_enemy(new_gamestate,new_gamestate.getPosition(self.index))):
+        if(self.is_in_enemy(new_gamestate,new_gamestate.getAgentPosition(self.index))):
             is_in_opp_ground=1
-        return {"num_opps_in_territory":sum_opps,"num_food_in_territory":friendly_food,"is_in_enemy":is_in_opp_ground}
+        min_dist_from_opp=min([self.distancer.getDistance(i,new_gamestate.getAgentPosition(self.index)) for i in [new_gamestate.getAgentPosition(k) for k in self.getOpponents(new_gamestate)]])
+        return {"num_opps_in_territory":sum_opps,"num_food_in_territory":friendly_food,"is_in_enemy":is_in_opp_ground,"min_dist":1/(min_dist_from_opp+1)}
