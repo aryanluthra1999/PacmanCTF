8b9cb78f243201af40331faa3ebf763177044e82
Richard Padilla <padillarichard2021@berkeley.edu>
2019-10-22 00:03:36 -0700

    added defensive agent skeleton

--- minicontest2/myTeam.py
+++ minicontest2/myTeam.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -381,6 +381,70 @@ class OffensiveAgent(CaptureAgent):
         '''
         CaptureAgent.registerInitialState(self, gameState)
 
+        '''
+        Your initialization code goes here, if you need any.
+        '''
+
+
+
+    def chooseAction(self, gameState):
+        """
+        Picks among actions randomly.
+        """
+        actions = gameState.getLegalActions(self.index)
+        actions = [a for a in actions if a != "Stop"]
+
+        '''
+        You should change this in your own agent.
+        '''
+        results = []
+        for action in actions:
+            results.append(self.evaluate(gameState, action))
+
+        optomizing_arg = np.argmax(results)
+        return actions[optomizing_arg]
+
+    def evaluate(self, gameState, action):
+        weights = self.getWeights(gameState, action)
+        features = self.getFeatures(gameState, action)
+
+        sum = 0
+        for k in weights.keys():
+            sum += weights[k] * features[k]
+
+        return sum
+
+    def getWeights(self, gameState, action):
+        # Set this manually
+
+        return {}
+
+    def getFeatures(self, gameState, action):
+        # figure out good features here
+
+        return {}
+class DefensiveAgent(CaptureAgent):
+
+    def registerInitialState(self, gameState):
+        """
+        This method handles the initial setup of the
+        agent to populate useful fields (such as what team
+        we're on).
+
+        A distanceCalculator instance caches the maze distances
+        between each pair of positions, so your agents can use:
+        self.distancer.getDistance(p1, p2)
+
+        IMPORTANT: This method may run for at most 15 seconds.
+        """
+
+        '''
+        Make sure you do not delete the following line. If you would like to
+        use Manhattan distances instead of maze distances in order to save
+        on initialization time, please take a look at
+        CaptureAgent.registerInitialState in captureAgents.py.
+        '''
+        CaptureAgent.registerInitialState(self, gameState)
         '''
         Your initialization code goes here, if you need any.
         '''
